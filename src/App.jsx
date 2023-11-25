353e44323f45811f71c781ca14dc549fadb176e1
Ansuofficial <badjieansu165@gmail.com>
2023-11-25 01:49:28 +0000

    Routes

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Hello App</h1>
+        </div>
+    )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import About from "./components/about.jsx"
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
